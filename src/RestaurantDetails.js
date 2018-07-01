1f5d451394e8eb5ed9007a0a0a0ce58d550ecc8a
nraovic <n.raovic@gmail.com>
2018-07-01 15:14:16 +0200

    Move cafeDetails state and setState to the main component - MapContainer

--- src/RestaurantDetails.js
+++ src/RestaurantDetails.js
@@ -6,45 +6,34 @@ import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
 
 export default class RestaurantDetails extends Component {
-  state = {
-    cafesDetails: {}
-  };
-  updateCafesDetails = id => {
-    if (!(id in this.state.cafesDetails)) {
-      getCafeDetails(id).then(results => {
-        const updatedDetails = Object.assign({}, this.state.cafesDetails);
-        updatedDetails[id] = results;
-        console.log(updatedDetails);
-        this.setState({ cafesDetails: updatedDetails });
-      });
-    }
-  };
+  state = {};
+
   componentDidMount() {
-    const { idUrl, redirect } = this.props;
-    this.updateCafesDetails(idUrl);
+    const { idUrl, redirect, updateCafesDetails } = this.props;
+    updateCafesDetails(idUrl);
   }
   render() {
-    const { idUrl, getDetails } = this.props;
+    const { idUrl, getDetails, cafesDetails } = this.props;
     const photoStyle = { maxWidth: '400px', maxHeight: '400px' };
     const url = window.location.href;
     const getIdfromUrl = url.substring(url.lastIndexOf('/') + 1);
-
+    console.log(cafesDetails);
     const photoUrl = url => {
-      const photoUrl = `${this.state.cafesDetails[url].venue.bestPhoto.prefix}${
-        this.state.cafesDetails[url].venue.bestPhoto.height
-      }x${this.state.cafesDetails[url].venue.bestPhoto.width}${this.state.cafesDetails[url].venue.bestPhoto.suffix}`;
+      const photoUrl = `${cafesDetails[url].venue.bestPhoto.prefix}${cafesDetails[url].venue.bestPhoto.height}x${
+        cafesDetails[url].venue.bestPhoto.width
+      }${cafesDetails[url].venue.bestPhoto.suffix}`;
       return photoUrl;
     };
     return (
       <div>
         <Link to="/">Go to results</Link>
-        {idUrl in this.state.cafesDetails && (
+        {idUrl in cafesDetails && (
           <div>
-            <h2>{this.state.cafesDetails[idUrl].venue.name}</h2>
-            <a href={this.state.cafesDetails[idUrl].venue.url} target="_blank">
-              <i className="fa fa-phone-square">{this.state.cafesDetails[idUrl].venue.url}</i>
+            <h2>{cafesDetails[idUrl].venue.name}</h2>
+            <a href={cafesDetails[idUrl].venue.url} target="_blank">
+              <i className="fa fa-phone-square">{cafesDetails[idUrl].venue.url}</i>
             </a>
-            <p>{this.state.cafesDetails[idUrl].venue.contact.phone}</p>
+            <p>{cafesDetails[idUrl].venue.contact.phone}</p>
             <img src={photoUrl(idUrl)} style={photoStyle} />
           </div>
         )}
