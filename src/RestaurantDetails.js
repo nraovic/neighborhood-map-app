72339b80373e0fd09986c72560f8bc6e0c6aa766
nraovic <n.raovic@gmail.com>
2018-07-13 23:39:25 +0200

    Show fatched data in Details

--- src/RestaurantDetails.js
+++ src/RestaurantDetails.js
@@ -4,26 +4,44 @@ import { getCafeDetails } from './Api/yelpApi.js';
 import update from 'immutability-helper';
 import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone, faGlobe, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 export default class RestaurantDetails extends Component {
   state = {};
-
-  componentDidMount() {
-    const { idUrl, redirect, updateCafesDetails } = this.props;
+  componentWillMount() {
+    const { idUrl, updateCafesDetails } = this.props;
     updateCafesDetails(idUrl);
   }
   render() {
-    const { idUrl, getDetails, cafesDetails, apiRequest } = this.props;
+    const { idUrl, cafesDetails, apiRequest } = this.props;
     const photoStyle = { maxWidth: '400px', maxHeight: '400px' };
-    const url = window.location.href;
-    const getIdfromUrl = url.substring(url.lastIndexOf('/') + 1);
-    console.log(cafesDetails);
+    // Get url for the cafe's photo
     const photoUrl = url => {
       const photoUrl = `${cafesDetails[url].venue.bestPhoto.prefix}${cafesDetails[url].venue.bestPhoto.height}x${
         cafesDetails[url].venue.bestPhoto.width
       }${cafesDetails[url].venue.bestPhoto.suffix}`;
       return photoUrl;
     };
+    // Define all data to be listed in the Details section.
+    let cafe, name, description, address, phone, currentlyOpen, timeFrames, rating, likes, websiteUrl;
+    if (idUrl in cafesDetails) {
+      cafe = cafesDetails[idUrl].venue;
+      console.log(cafe);
+      name = cafe.name;
+      description = cafe.description;
+      address = cafe.location.address;
+      phone = cafe.contact.formattedPhone;
+      if (cafe.popular.isOpen) {
+        currentlyOpen = 'Open';
+      } else {
+        currentlyOpen = 'Closed';
+      }
+      rating = `${cafe.rating}/10`;
+      likes = cafe.likes.count;
+      websiteUrl = cafe.url;
+      timeFrames = cafe.popular.timeframes;
+    }
     return (
       <div>
         <Link to="/">Go to results</Link>
@@ -31,13 +49,34 @@ export default class RestaurantDetails extends Component {
         {apiRequest && (
           <div>We are sorry. We could not get the data about the Cafe from Foursquare. Please try again later.</div>
         )}
+        {/*Check first if the data has been updated in Cafe Details*/}
         {idUrl in cafesDetails && (
           <div>
-            <h2>{cafesDetails[idUrl].venue.name}</h2>
-            <a href={cafesDetails[idUrl].venue.url} target="_blank">
-              <i className="fa fa-phone-square">{cafesDetails[idUrl].venue.url}</i>
+            <h2>{name}</h2>
+            <p>{description}</p>
+            <a href={websiteUrl} target="_blank">
+              <FontAwesomeIcon icon={faGlobe} />
+              {websiteUrl}
             </a>
-            <p>{cafesDetails[idUrl].venue.contact.phone}</p>
+            <p>
+              <FontAwesomeIcon icon={faPhone} />
+              {phone}
+            </p>
+            <p>
+              <FontAwesomeIcon icon={faMapMarkerAlt} />
+              {address}
+            </p>
+            <p>Rating: {rating}</p>
+            <p>Hours: {currentlyOpen}</p>
+            {/*Extract the opening hours by days*/}
+            <div>
+              {timeFrames.map(frame => (
+                <div>
+                  <span>{frame.days}</span>
+                  {frame.open.map(time => <span>{time.renderedTime}</span>)}
+                </div>
+              ))}
+            </div>
             <img src={photoUrl(idUrl)} style={photoStyle} />
           </div>
         )}
